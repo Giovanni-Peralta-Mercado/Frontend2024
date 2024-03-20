4489380b9fa388c32d24022a1f19e90643afd84e
Giovanni-Peralta-Mercado <isc20350543@gmail.com>
2024-03-19 19:41:23 -0600

    Inputs

--- 08-Inputs/src/App.jsx
+++ 08-Inputs/src/App.jsx
@@ -1,47 +1,66 @@
 
-
 import { useState } from "react"
 
 function App() {
-  const [username, setUsername] = useState("")
-  const handleUsernameChange = (event) =>{
-    setUsername(event.target.value)
+
+  const [inputsForm,setImputsForm]=useState({
+    username:"",
+    password:""
+  })
+
+
+  const handleInputChange=(event)=>{
+    setImputsForm({
+      ...inputsForm,
+      [event.target.name]:event.target.value
+    })
   }
 
-  const handleSubmit = (e) => {
+
+  const handleSubmit=(e)=>{
     e.preventDefault()
-    console.log("Mi nombre de usuario es ", username)
-    setUsername("")
+    console.log("Mi usuario es: ",inputsForm.username)
+    console.log("Mi contraseña es: ",inputsForm.password)
+    setImputsForm({
+      username:"",
+      password:""
+    })
+    
   }
-
 
   return (
     <div>
-      <h1>Inputs</h1>
+      <h1>Input</h1>
       <hr />
-      <form onSubmit= {(e) => handleSubmit(e)}>
-        <div>
-          <label htmlFor="username"> Username </label>
-          <input
-            id="username"
-            type="text"
-            value= {username}
-            onChange={(event) => handleUsernameChange(event)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password"> Password </label>
-            <input
-              id="password"
-              type="password"
-          />
-        </div>
-        <div>
-          <button type="submit"> Submit </button>
-        </div>
-      </form>
+    <form onSubmit={(e)=>handleSubmit(e)}>
+      <div>
+        <label htmlFor="username"> Username </label>
+        <input 
+        id="username" 
+        name="username"
+        type="text" 
+        value={inputsForm.username}
+        onChange={(event)=>handleInputChange(event)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password"> Password </label>
+        <input 
+        id="password" 
+        name="password"
+        type="password" 
+        value={inputsForm.password}
+        onChange={(event)=>handleInputChange(event)}
+        />
+      </div>
+      <div>
+        <button type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
     </div>
-      
+    
   )
 }
 
