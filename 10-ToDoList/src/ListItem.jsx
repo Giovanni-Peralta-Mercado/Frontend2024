b392bac807cbdeb991ead95d44aa6b77b6f803e1
Giovanni-Peralta-Mercado <isc20350543@gmail.com>
2024-04-29 18:57:18 -0600

    To Do List

---
+++ 10-ToDoList/src/ListItem.jsx
@@ -0,0 +1,32 @@
+
+import ShowItemModal from "./ShowItemModal"
+const ListItem = ({task, setTaskList, taskList}) =>{
+
+return(
+    <div>
+    <div className="row m-2">
+    <div className="col-6">
+      <ShowItemModal 
+      task={task} 
+      taskList={taskList}
+      setTaskList={setTaskList}/>
+        <input type="checkbox"/>
+          <button 
+          className="btn btn-link"
+          data-bs-toggle="modal"
+          data-bs-target={"#ShowItemModal"+task.id}
+          >{task.task}</button>
+        </div>
+        <div className="col-3">
+          {task.limit}
+        </div>
+        <div className="col">
+          {task.location}
+        </div>
+        </div>
+        </div>
+
+)
+}
+
+export default ListItem
