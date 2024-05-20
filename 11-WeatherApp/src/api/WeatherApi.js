4bddfd503db6602ab5646c95db68e026386e5ccd
Giovanni-Peralta-Mercado <isc20350543@gmail.com>
2024-05-19 20:23:17 -0600

    WeatherApp

--- 11-WeatherApp/src/api/WeatherApi.js
+++ 11-WeatherApp/src/api/WeatherApi.js
@@ -1,6 +1,6 @@
-export const getWeather = async () => {
+export const getWeather = async (city) => {
     const key = "dddee20504cc4d43a0e221216241205"
-    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=Tuxtepec&aqi=yes`
+    const url = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${city}&aqi=yes&lang=es`
     const response = await fetch(url)
     const weather = await response.json()
 
